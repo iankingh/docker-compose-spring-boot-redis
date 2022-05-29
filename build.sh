bca49402bc0e911050f530652e418000df3054ff
ian <ianaa10716@gmail.com>
2022-05-30 01:41:27 +0800

    更新為war

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ cd spring-boot-redis
 
 ./mvnw clean package
 
-cp ./target/spring-boot-redis-0.0.1-SNAPSHOT.jar  ../tomcat/webapps/
+cp ./target/spring-boot-redis-0.0.1-SNAPSHOT.war  ../tomcat/webapps/
 
 cd ..
 
